7c7702dd51077c5be21b01797741708579efbb35
gj026621 <116082443+gj026621@users.noreply.github.com>
2024-08-18 14:30:37 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -7,16 +7,34 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "1.1",
   "title": "Conversions",
-  "body": " Conversions  Bla.   Bla.    Facteurs de conversions  Bla.   "
+  "body": " Conversions   Facteurs de conversion  Multiplier une quantité par 1 ne la modifie pas. Cette astuce permet de faire apparaître l'unité souhaitée lors d'une conversion.     Convertissez 12 gigamètres en micromètres. Utilisez la notation scientifique pour exprimer votre réponse.               Bla.      Combien y a-t-il de pieds dans verges? Utilisez la notation décimale pour exprimer votre réponse.          pieds    Bla.      Combien y a-t-il de micromètres dans milles? Utilisez la notation scientifique pour exprimer votre réponse. Arrondissez la mantisse à deux décimales.               Bla.    "
 },
 {
-  "id": "sec-section-conversions-3",
+  "id": "ex-conversion-1",
   "level": "2",
-  "url": "sec-section-conversions.html#sec-section-conversions-3",
-  "type": "Example",
+  "url": "sec-section-conversions.html#ex-conversion-1",
+  "type": "Exemple",
   "number": "1.1.1",
   "title": "",
-  "body": " Bla.  "
+  "body": "  Convertissez 12 gigamètres en micromètres. Utilisez la notation scientifique pour exprimer votre réponse.               Bla.   "
+},
+{
+  "id": "ex-conversion-2",
+  "level": "2",
+  "url": "sec-section-conversions.html#ex-conversion-2",
+  "type": "Exemple",
+  "number": "1.1.2",
+  "title": "",
+  "body": "  Combien y a-t-il de pieds dans verges? Utilisez la notation décimale pour exprimer votre réponse.          pieds    Bla.   "
+},
+{
+  "id": "ex-conversion-3",
+  "level": "2",
+  "url": "sec-section-conversions.html#ex-conversion-3",
+  "type": "Exemple",
+  "number": "1.1.3",
+  "title": "",
+  "body": "  Combien y a-t-il de micromètres dans milles? Utilisez la notation scientifique pour exprimer votre réponse. Arrondissez la mantisse à deux décimales.               Bla.   "
 }
 ]
 
